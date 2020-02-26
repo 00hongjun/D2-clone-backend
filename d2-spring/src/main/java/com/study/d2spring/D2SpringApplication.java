a1c2cb19d52d08bfc731dc80cf51b27c9a54d3a6
chj <hongjc423@gmail.com>
2020-02-26 21:58:47 +0900

    first commit

---
+++ d2-spring/src/main/java/com/study/d2spring/D2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.study.d2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2SpringApplication.class, args);
+	}
+
+}
---
+++ d2-spring/src/test/java/com/study/d2spring/D2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.d2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
