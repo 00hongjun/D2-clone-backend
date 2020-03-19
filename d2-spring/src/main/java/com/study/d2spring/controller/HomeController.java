6ed9757dbf56d1aa7b7f917d82c7d9a3c0984afc
chj <hongjc423@gmail.com>
2020-03-19 21:04:21 +0900

    home 컨트롤러 수정

--- d2-spring/src/main/java/com/study/d2spring/controller/HomeController.java
+++ d2-spring/src/main/java/com/study/d2spring/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.study.d2spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String home() {
         return "hello!";
     }
