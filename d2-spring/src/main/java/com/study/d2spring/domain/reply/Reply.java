adae95bb42f2370192f4728a3b66cfb4f5d21148
yangjin-yun <ess234@naver.com>
2020-03-30 21:26:35 +0900

    엔티티 정의

--- d2-spring/src/main/java/com/study/d2spring/domain/reply/Reply.java
+++ d2-spring/src/main/java/com/study/d2spring/domain/reply/Reply.java
@@ -1,4 +1,33 @@
 package com.study.d2spring.domain.reply;
 
+import com.study.d2spring.domain.post.Post;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "d2_post_reply")
 public class Reply {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "reply_id")
+    private Long id;
+
+    private String text;
+    private String replyer;
+    private LocalDateTime replyAt;
+    private int goodCount;
+    private int badCount;
+    private String comments;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
 }
--- d2-spring/src/main/java/com/study/d2spring/domain/tag/Tag.java
+++ d2-spring/src/main/java/com/study/d2spring/domain/tag/Tag.java
@@ -1,7 +1,27 @@
 package com.study.d2spring.domain.tag;
 
-import javax.persistence.Entity;
+import com.study.d2spring.domain.post.Post;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@Getter
+@Table(name = "d2_tag")
 public class Tag {
+
+    @Id @GeneratedValue
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String name;
+    private String url;
+
+    @OneToMany
+    private List<Post> posts = new ArrayList<>();
 }
