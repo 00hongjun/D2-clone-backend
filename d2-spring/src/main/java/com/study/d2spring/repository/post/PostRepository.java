dbce3735a9684bda7c159936832e271f4738052d
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-09 20:49:46 +0900

    Post Entity 전체 조회 추가 (PostRepository.java)

--- d2-spring/src/main/java/com/study/d2spring/repository/post/PostRepository.java
+++ d2-spring/src/main/java/com/study/d2spring/repository/post/PostRepository.java
@@ -1,9 +1,6 @@
 package com.study.d2spring.repository.post;
 
 import com.mysema.query.jpa.impl.JPAQuery;
-import com.mysema.query.jpa.impl.JPAQueryFactory;
-import com.study.d2spring.domain.member.Member;
-import com.study.d2spring.domain.member.QMember;
 import com.study.d2spring.domain.member.QPosting;
 import com.study.d2spring.domain.post.Post;
 import com.study.d2spring.domain.post.QCategory;
@@ -11,22 +8,20 @@ import com.study.d2spring.domain.post.QPost;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class PostRepository {
-//
+
     @Autowired
     private final EntityManagerFactory emf;
     private final EntityManager em;
 
-    public List<Post> findAll(){
+    public List<Post> findAll() {
         EntityManager em = emf.createEntityManager();
         JPAQuery query = new JPAQuery(em);
 
@@ -42,7 +37,7 @@ public class PostRepository {
         return posts;
     }
 
-    public Post findOneByCategory(Long _postId, Long _categoryId){
+    public Post findOneByCategory(Long _postId, Long _categoryId) {
         EntityManager em = emf.createEntityManager();
         JPAQuery query = new JPAQuery(em);
 
@@ -58,4 +53,15 @@ public class PostRepository {
         return post;
     }
 
+    public List<Post> findPostAll() {
+        EntityManager em = emf.createEntityManager();
+        JPAQuery query = new JPAQuery(em);
+
+        QPost qPost = QPost.post;
+
+        List<Post> posts = query.from(qPost)
+                .list(qPost);
+
+        return posts;
+    }
 }
