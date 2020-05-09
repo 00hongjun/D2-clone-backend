79b8f3e73707528f56b8e3436b8c6a99054be579
yangjin-yun <ess234@naver.com>
2020-05-10 02:09:29 +0900

    tag 데이터 수정 쿼리문

---
+++ mysql/update_tag_postId.sql
@@ -0,0 +1,4 @@
+update  d2_tag_post set post_id = '650' where tag_id = '3'; 
+update  d2_tag_post set post_id = '649' where tag_id = '5'; 
+update  d2_tag_post set post_id = '648' where tag_id = '6'; 
+update  d2_tag_post set post_id = '644' where tag_id = '4'; 
